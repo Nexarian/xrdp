6703299ea3835ae7edcd38ac38f64db6826caddb
Christopher Pitstick <cmp@pitstick.net>
2018-10-15 21:42:13 -0700

    Resolution switching without reconnecting.
    - Based on https://github.com/jsorg71/xrdp/tree/dynamic_monitor
    - Tested with xorgxrdp
    - Tested with vnc
    - Only works with single monitor.
    - Update documentation to clarify the difference between MSTSC and
    Microsoft Remote Desktop.
    - Compatible with NVENC and xorgxrdp_helper
    - Compatible with Nvidia.
    - Updates to include ms-rdpedisp.h header for the 2.2.2 specification of
    the protocol.
    - State machine for resizing.
    - Mechanisms to make sure the key frame is always sent on resize.
    - Adds new struct that shares the number of monitors with xrdp_client_info.h
    - Modification to the original resize setup that works with /gfx.
    - YAMI compat.
    - Updating to librfxcodec branch that is also a merge from mainline.
    - Made sure RFX progressive works.
    
    Depends on https://github.com/neutrinolabs/xorgxrdp/pull/183

--- scripts/install_xrdp_build_dependencies_with_apt.sh
+++ scripts/install_xrdp_build_dependencies_with_apt.sh
@@ -77,8 +77,9 @@ in
             libssl-dev \
             libx11-dev \
             libxrandr-dev \
-            libxfixes-dev"
-
+            libxfixes-dev \
+            libepoxy-dev \
+            libepoxy0"
         case "$FEATURE_SET"
         in
             min)
@@ -109,6 +110,7 @@ in
             $LIBFREETYPE_DEV:i386 \
             libgl1-mesa-dev:i386 \
             libglu1-mesa-dev:i386 \
+            libegl1-mesa-dev:i386 \
             libjpeg-dev:i386 \
             libimlib2-dev:i386 \
             libmp3lame-dev:i386 \
@@ -121,8 +123,9 @@ in
             libxfixes-dev:i386 \
             libxrandr-dev:i386 \
             libxrender-dev:i386 \
-            libfuse-dev:i386"
-
+            libfuse-dev:i386 \
+            libepoxy-dev:i386 \
+            libepoxy0:i386"
         dpkg --add-architecture i386
         dpkg --print-architecture
         dpkg --print-foreign-architectures
